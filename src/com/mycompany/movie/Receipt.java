c29807375f08e6cb0db2e126cab0612c942e6d95
Blueaaaaaaaa <Hailam04.work@gmail.com>
2024-11-19 10:50:52 +0700

    Keep Streak

--- src/com/mycompany/movie/Receipt.java
+++ src/com/mycompany/movie/Receipt.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.movie;
+package termproject.src.com.mycompany.movie;
 
 import java.io.File;
 import java.io.FileNotFoundException;
